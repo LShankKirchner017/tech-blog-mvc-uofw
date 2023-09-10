109c767f93efeba8b63937135270325177391ed1
Lindsay <lshank017@gmail.com>
2023-09-10 12:00:09 -0500

    started controllers, models, views

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const routes = require('./controllers')
-const exphbs = require('express-handlebars')
+
 
 const sequelize = require('./config/connection')
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
 
 const app = express()
 const PORT = process.env.PORT || 3003;
